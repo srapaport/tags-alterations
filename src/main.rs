c453e2330d3e4a47de09f76f3aa7c345ed5fbf92
Solal <solal.rapaport@telecom-paris.fr>
2026-01-12 16:15:09 +0100

    Add dependencies and implement graph loading in main function
    - Updated Cargo.toml to include `anyhow`, `dotenv`, and `swh-graph` dependencies.
    - Modified main.rs to load environment variables and initialize a bidirectional graph using the SWH library.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use dotenv::dotenv;
+use std::env;
+use swh_graph::graph::*;
+use swh_graph::mph::DynMphf;
+fn main() -> Result<()> {
+    dotenv()?;
+    let graph_basename = env::var("GRAPH_BASENAME").expect("GRAPH_BASENAME not set");
+    let graph = SwhBidirectionalGraph::new(graph_basename)?
+        .load_all_properties::<DynMphf>()?
+        .load_forward_labels()?
+        .load_backward_labels()?;
+    Ok(())
 }
